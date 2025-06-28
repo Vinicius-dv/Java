30a946d3629c7fa391560df97017920339666228
Vinicius-dv <150695214+Vinicius-dv@users.noreply.github.com>
2025-06-28 20:20:40 -0300

    Aprendendo java

---
+++ Ex001/Main.java
@@ -0,0 +1,6 @@
+package Ex001;
+public class Main{
+   public static void main(String[] args) {
+      System.out.println("Olá mundo");
+   }
+}
---
+++ Ex002/Main.java
@@ -0,0 +1,6 @@
+package Ex002;
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("olá mundo");
+    }
+}
