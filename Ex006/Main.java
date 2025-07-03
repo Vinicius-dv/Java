0b0d4c18ee26216fef267741e3d3d0a2f27610eb
Vinicius-dv <150695214+Vinicius-dv@users.noreply.github.com>
2025-07-03 11:43:30 -0300

    Aprendendo java

---
+++ Ex006/Main.java
@@ -0,0 +1,10 @@
+package Ex006;
+
+public class Main {
+    public static void main(String[] args) {
+        float n1 = (float) 4.3;
+        float n2 = (float) 2;
+        float media = (float) (n1+n2)/2;
+        System.out.println((media<5)?"Reprovado":"Aprovado");
+    }
+}
---
+++ Ex007/Main.java
@@ -0,0 +1,10 @@
+package Ex007;
+
+public class Main {
+    public static void main(String[] args) {
+        float n1 = (float) 4.3;
+        float n2 = (float) 2;
+        float media = (float) (n1+n2)/2;
+        System.out.println(media);
+    }
+}
