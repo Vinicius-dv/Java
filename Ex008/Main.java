fbac6a2a7d85403168c58446d0bbc268b9876369
Vinicius-dv <150695214+Vinicius-dv@users.noreply.github.com>
2025-07-04 13:53:48 -0300

    Aprendendo java

---
+++ Ex008/Main.java
@@ -0,0 +1,14 @@
+package Ex008;
+
+public class Main {
+    public static void main(String[] args) {
+        int idade_int = 16;
+        double salario_double = 2000;
+        float salario_float = 2500;
+        byte idade_byte = 127;
+        short idade_short = 18;
+        long num_long =  10000;
+        char str = 'M';
+        System.out.println(str);
+    }
+}
