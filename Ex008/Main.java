b2d43e6201ff834e690beff2e9aaa11427e13586
Vinicius-dv <150695214+Vinicius-dv@users.noreply.github.com>
2025-07-06 20:02:31 -0300

    Aprendendo casting em java

--- Ex008/Main.java
+++ Ex008/Main.java
@@ -3,12 +3,12 @@ package Ex008;
 public class Main {
     public static void main(String[] args) {
         int idade_int = 16;
-        double salario_double = 2000;
-        float salario_float = 2500;
+        double salario_double = 2000.09;
+        float salario_float =  /*(Float)*/ 2500.50f;
         byte idade_byte = 127;
         short idade_short = 18;
-        long num_long =  10000;
+        long num_long =  (int) 10000000000000L;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
         char str = 'M';
-        System.out.println(str);
+        System.out.println(num_long);
     }
 }
